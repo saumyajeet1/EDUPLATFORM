294d40b63d76440ce4ec41c481d208aef9e8f95b
saumyajeet1 <46256820+saumyajeet1@users.noreply.github.com>
2020-03-31 12:37:42 +0530

    k

--- myapp/src/components/main/main.js
+++ myapp/src/components/main/main.js
@@ -9,46 +9,14 @@ import { Grid } from '@material-ui/core';
 import flow from "../../images/flow.png"; 
 import aut1 from "../../images/aut1.png";
 import aut2 from "../../images/aut2.png";
-import Aux from "../hoc/Aux";
+// import Aux from "../hoc/Aux";
 import { Container, Row, Col, Jumbotron, Button, Image } from 'react-bootstrap';
 class Main extends Component {
-
-    state={
-        news:'',
-    }
-    
-  //    newslist=()=>(
-  //        this.state.news?
-       
-  //        this.state.news.map((item,i)=>(        
-  // <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
-  // <div className="d-flex w-100 justify-content-between">
-  //   <h5 className="mb-1">CREATED AT {item.createdAt}</h5>
-  
-  // </div>
-  //    <p className="mb-1">{item.subject}</p>
-  //    <p className="mb-1">BY {item.author}</p>
-  // </a>
-  //        ))
-  //        :null
-  //       )
-      
-
-  //   componentWillMount(){
-  //       this.props.dispatch(allnews()).then(response=>{
-  //           console.log(response.payload)
-  //           this.setState({
-  //               news:response.payload
-  //           })
-  //       }).catch(err=>console.log(err))
-  //   }
-
-
 
     render() {
         var height="560px";
         return (
-          <Aux>
+          <div>
               <Container style={{marginTop:"20px"}}>
             <Row >
     
@@ -182,131 +150,9 @@ class Main extends Component {
             </Row>
           </Container>
 
-          </Aux>
+           </div>
           
 
-//             <div className="container">
-             
-//         <div id="myCarousel" className="carousel slide" data-ride="carousel" style={{objectFit:"cover"}}>
-//           <ol className="carousel-indicators">
-//             <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
-//             <li data-target="#myCarousel" data-slide-to="1"></li>
-//             <li data-target="#myCarousel" data-slide-to="2"></li>
-//           </ol>
-//           <div className="carousel-inner">
-      
-//             <div className="item active" style={{objectFit:"cover"}}>
-//               <img src={four} alt="eduplat" />
-//               <div className="carousel-caption">
-//               </div>
-//             </div>
-      
-//             <div className="item">
-//               <img src={five} alt="eduplat"/>
-//               <div className="carousel-caption">
-//               </div>
-//             </div>
-          
-//             <div className="item" style={{objectFit:"cover"}}>
-//               <img src={six} alt="eduplat"/>
-//               <div className="carousel-caption">
-//               </div>
-//             </div>
-        
-//           </div>
-      
-//           <a className="left carousel-control" href="#myCarousel" data-slide="prev">
-//             <span className="glyphicon glyphicon-chevron-left"></span>
-//             <span className="sr-only">Previous</span>
-//           </a>
-//           <a className="right carousel-control" href="#myCarousel" data-slide="next">
-//             <span className="glyphicon glyphicon-chevron-right"></span>
-//             <span className="sr-only">Next</span>
-//           </a>
-        
-//       </div>
-            
-            
-//               <div className="row" id="about">
-               
-//                <div className="col-lg-12 col-sm-12 col-md-12 description" >
-                   
-//                <h1 style={{color:"black"}}>ABOUT CHESS</h1>
-//                    <div className="circle">
-    
-//                    </div>
-//                    <p style={{fontFamily: "'Lobster Two', cursive"}}>The Chemical Engineering Student's Society (CHESS) is an independent student run society of Chemical Engineering Department of NIT Durgapur.It is solely dedicated to form a network
-//                      among all the students,faculty members and alumni of the department. It promotes interaction  within the department and implements acadmeic, social and other programs of interest to its members.
-                   
-//                    </p>
-               
-               
-//                </div>
-//                </div>
-//               <div className="row">
-//                <div id="box">
-//                 <div id="smallbox">
-//                     <i className="fas fa-user-graduate" id="cap1" style={{fontSize:"40px",color:"#E39821"}}>
-//                         <h3 id="faculty"><Link to="/facad">FACULTY</Link></h3></i>
-//                 </div>
-//                 <div id="smallbox">
-//                     <i className="fas fa-graduation-cap" id="cap1" style={{fontSize:"40px",color:"#E39821"}}>
-//                         <h3 id="faculty"><Link to="/subject">SYLLABUS</Link></h3></i>
-//                </div>
-               
-//                <div id="smallbox">
-//                 <i className="fa fa-book" id="cap1"style={{fontSize:"40px",color:"#E39821"}}>
-//                 <h3 id="faculty"><Link to="/register">REGISTER</Link></h3></i>
-//                </div>
-//                <div id="smallbox">
-//                 <i className="fa fa-lock" id="cap1" style={{fontSize:"40px",color:"#E39821"}}>
-//                 <h3 id="faculty"><Link to="/login">LOGIN</Link></h3></i>
-//                </div>
-                 
-//             </div>
-//              </div>
-//              <div className="row" id="about">
-               
-//                <div className="col-lg-12 col-sm-12 col-md-12 description" >
-                   
-//                <h1 style={{color:"black"}}>MESSAGE FROM HOD:Prof Gopinath Halder</h1>
-//                    <div className="circler">
-    
-//                    </div>
-               
-//                    <p style={{fontFamily: "'Lobster Two', cursive"}}>Chemical Engineering Students'society is working with a motivation of developing professional
-//     and personal skills of students,which facilitate them to perform magnificently as an eminent
-//     engineer in the society. This society is aimed to provide a platform to showcase and sharpen
-//     student's talents through a variety of events and activities planned throughout four years,
-//     for overall development of students personalities. We continue our effort to prepare and
-//     guide the students for the various competitive exams and interviews so that they perform
-//     better and make their carrier in the selected fields. Real world problems will be blended in
-//     the curriculum and these are to be taught by experts from industry, in addition through guest
-//     lectures. Our focus is to effectively train our students as chemical engineers who can serve the
-//     society competently, collaboratively and ethically as planners, designers and operators of the
-//     environment.
-//                    </p>
-               
-               
-//                </div>
-            
-//             </div>
-//             <br></br>
-//             <br></br>
-//             <br></br>
-// {/*             
-//             <h1 style={{color:"black"}}>NEWS LIST</h1>
-//             <div className="row">
-              
-//             <br></br><br></br><br></br> <br></br><br></br><br></br> <br></br><br></br><br></br>
-//             <div className="list-group">
-//             { this.newslist()}
-//             </div>
-              
-//         </div> */}
-//             </div>
-        
-               
         );
     }
 }
